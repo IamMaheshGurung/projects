ef30d63e7b93c2d76a4be7983e463eb2eba4d286
IamMaheshGurung <gurungmahesh167@gmail.com>
2024-11-15 17:48:40 +0900

    added add button on the form

--- hotelInventory/controllers/itemController.go
+++ hotelInventory/controllers/itemController.go
@@ -18,30 +18,37 @@ import(
 
 //Functions which will render inventory list
 
-func ShowInventory(w http.ResponseWriter, r * http.Request){
+func ShowInventory(w http.ResponseWriter, r *http.Request) {
     var items []models.Item
 
-
+    // Query database for items
     result := initializers.DB.Find(&items)
     if result.Error != nil {
+        log.Printf("Error fetching items: %v", result.Error)
         http.Error(w, result.Error.Error(), http.StatusInternalServerError)
         return
     }
 
+    log.Printf("Items found: %d", len(items))
 
-
-//for HTML page rendering with templates
+    // Parse and render template
     tmpl, err := template.ParseFiles("templates/index.html")
     if err != nil {
-        log.Printf("Unable to get the templates %s", err)
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return 
+        log.Printf("Unable to parse template: %v", err)
+        http.Error(w, "Template error: "+err.Error(), http.StatusInternalServerError)
+        return
     }
-    tmpl.Execute(w, struct {
+
+    err = tmpl.Execute(w, struct {
         Items []models.Item
     }{Items: items})
-}
 
+    if err != nil {
+        log.Printf("Error executing template: %v", err)
+        http.Error(w, "Execution error: "+err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
 
 func CreateInventory(w http.ResponseWriter, r * http.Request) {
     if r.Method == http.MethodPost {
