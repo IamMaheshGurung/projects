34865968554a239759651104ad9dcb03b1a64c4f
IamMaheshGurung <gurungmahesh167@gmail.com>
2024-11-18 17:27:02 +0900

    ..

--- hotelInventory/controllers/itemController.go
+++ hotelInventory/controllers/itemController.go
@@ -282,10 +282,12 @@ func DeleteItem(w http.ResponseWriter, r *http.Request) {
                 http.Error(w, "Error deleting item", http.StatusInternalServerError)
                 return
             }
+        http.Redirect(w, r, "/", http.StatusFound)
+
         }
 
         // Redirect to the inventory list after deletion or cancellation
-        http.Redirect(w, r, "/inventory", http.StatusFound)
+        http.Redirect(w, r, "/", http.StatusFound)
     }
 }
 
