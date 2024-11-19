d7ee83e44ef586f57c68c772e686f1ae97251f5b
IamMaheshGurung <gurungmahesh167@gmail.com>
2024-11-19 20:17:24 +0900

    added the part of middleware

--- hotelInventory/middleware/middleware.go
+++ hotelInventory/middleware/middleware.go
@@ -1,1 +1,31 @@
+package middleware
 
+
+import(
+
+    "net/http"
+    "os"
+    "time"
+    "log"
+    "fmt"
+    "github.com/golang-jwt/jwt/v5"
+    "context"
+)
+
+
+
+type contextKey string 
+
+
+const userContextKey = contextKey("user")
+
+func RequireAuth(next http.Handler) http.Handler{
+    return http.HandlerFunc(func(w http.ResponseWriter, r * http.Request){
+        tokenCookie, err := r.Cookie("Authorization")
+        if err != nil {
+            log.Printf("Unable to get the token string")
+        }
+        tokenString  := token.Value
+
+    }
+}
