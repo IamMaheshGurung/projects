2f13a269f20acb5a97a5bc7c7b869001f9eea794
IamMaheshGurung <gurungmahesh167@gmail.com>
2024-11-22 15:56:20 +0900

    added error controller func

---
+++ hotelInventory/controllers/errorController.go
@@ -0,0 +1,27 @@
+package controllers
+
+
+
+import (
+    "log"
+    "net/http"
+    "html/template"
+
+
+)
+
+
+func RenderError(w http.ResponseWriter, errorMessage string){
+    tmpl, err := template.ParseFiles("templates/error.html")
+    if err != nil {
+        log.Printf("Unable to render the error %s", err)
+        return 
+    }
+    w.Header().Set("Content-Type", "text/html")
+    err = tmpl.Execute(w, errorMessage)
+    if err != nil {
+        log.Printf("Unable to execute the html")
+        return 
+    }
+}
+
